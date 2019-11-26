e502c596375a4e957fd06efec3e8ffc8894a18a5
Justin <jadams5@mail.umw.edu>
2019-11-26 11:14:37 -0500

    Updates

--- PipBoy500.py
+++ PipBoy500.py
@@ -1,49 +1,48 @@
 import PySimpleGUI as sg # ------ GUI 
 import time
-from Radio import Radio
+from Radio import RadioDevice
 
-radio = Radio()
+radio = RadioDevice()
 
 sg.ChangeLookAndFeel('GreenTan')
 sg.SetOptions(element_padding=(0, 0))
 
+screenSize = (800, 480) # -- Display size
+stdFontSize = 30 # -- Standard font size
+buttFontSize = 20 # -- Button font size
 currTime = time.asctime( time.localtime(time.time()) )
 currTemp = 76
 currHumidity = 40
 currPressure = 80
 radioActive = False
 envActive = False
-
+mapActive = False
 
 # ------ Update the time and readings ------
 def updateAll():
     currTime = time.asctime( time.localtime(time.time()) )
     window.FindElement('clock').Update(currTime)
-def radioUpdateAll():
-    currTime = time.asctime( time.localtime(time.time()) )
-    radioWindow.FindElement('clock').Update(currTime)
-    radioWindow.FindElement('currSong').Update('Playing: %s' %radio.currSongName)
-
-def environmentUpdate():
-    currTime = time.asctime( time.localtime(time.time()) )
-    environmentWindow.FindElement('clock').Update(currTime)
-    environmentWindow.FindElement('temperature').Update('Temperature: %d°' %currTemp)
-    environmentWindow.FindElement('humidity').Update('Humidity: %d' %currHumidity)
-    environmentWindow.FindElement('pressure').Update('Atm. Pressure: %d' %currPressure)
-
+    if radioActive:
+        radioWindow.FindElement('clock').Update(currTime)
+        radioWindow.FindElement('currSong').Update('Playing: %s' %radio.currSongName)
+    if envActive:
+        environmentWindow.FindElement('clock').Update(currTime)
+        environmentWindow.FindElement('temperature').Update('Temperature: %d°' %currTemp)
+        environmentWindow.FindElement('humidity').Update('Humidity: %d' %currHumidity)
+        environmentWindow.FindElement('pressure').Update('Atm. Pressure: %d' %currPressure)
+    if mapActive:
+        mapWindow.FindElement('clock').Update(currTime)
     
 
 def getRadioLayout():
     musicFrame =    [
-                    [sg.Text('Playing: %s' %radio.currSongName, key='currSong')],
-                    [sg.T()],
-                    [sg.T()],
-                    [sg.Button('Radio', button_color=('green', 'pink'), key='Radio'), sg.Button('Play', button_color=('green', 'pink'), key='Play'), sg.T(), sg.Button('Stop', button_color=('red', 'white'), key='Stop'), sg.T(), sg.Button('Next', button_color=('green', 'pink'), key='Next')]
-                ]
+                        [sg.Text('Playing: %s' %radio.currSongName, key='currSong', font=stdFontSize)],
+                        [sg.Button('Prev', button_color=('green', 'pink'), key='Prev'),
+                        sg.Button('Play', button_color=('green', 'pink'), key='Play'), sg.Button('Stop', button_color=('red', 'white'), key='Stop'), sg.Button('Next', button_color=('green', 'pink'), key='Next')],
+                    ]
     radioLayout =   [
-                    [sg.Text('Username', font=('Arial', 10))],
-                    [sg.Text(currTime, key='clock', font=('Arial', 15)), sg.T(' ' * 30), sg.Button('Return', button_color=('white', 'firebrick4'), key='Return')],
-                    [sg.T()],
+                    [sg.Text('Username', font=('Arial', stdFontSize))],
+                    [sg.Text(currTime, key='clock', font=('Arial', stdFontSize)), sg.Button('Return', button_color=('white', 'firebrick4'), key='Return')],
                     [sg.Frame('Radio', musicFrame)],
                 ]
     return radioLayout
@@ -55,32 +54,39 @@ def getEnvironmentLayout():
                             [sg.Text('Atm. Pressure: %d' %currPressure, key='pressure')]
                         ]
     environmentLayout = [
-                            [sg.Text('Username', font=('Arial', 10))],
-                            [sg.Text(currTime, key='clock', font=('Arial', 15)), sg.T(' ' * 30), sg.Button('Return', button_color=('white', 'firebrick4'), key='Return')],
-                            [sg.T()],
+                            [sg.Text('Username', font=('Arial', stdFontSize))],
+                            [sg.Text(currTime, key='clock', font=('Arial', stdFontSize)), sg.Button('Return', button_color=('white', 'firebrick4'), key='Return')],
                             [sg.Frame('Environment', environmentFrame)],
                         ]
     return environmentLayout
 
+def getMapLayout():
+    mapLayout = [
+                    [sg.Text('Map Window')],
+                    [sg.Text('Username', font=('Arial', stdFontSize))],
+                    [sg.Text(currTime, key='clock', font=('Arial', stdFontSize)), sg.Button('Return', button_color=('white', 'firebrick4'), key='Return')],
+                ]
+    return mapLayout
+
 # ------ Layout Mapping ------
 
 layout =    [   
-                [sg.Text('Username', font=('Arial', 10))],
-                [sg.Text(currTime, key='clock', font=('Arial', 15))],
+                [sg.Text('Username', font=('Arial', stdFontSize))],
+                [sg.Text(currTime, key='clock', font=('Arial', buttFontSize))],
                 [sg.T()],
-                [sg.Button('Environment', button_color=('white', 'blue'), key='Environment')],
+                [sg.Button('Environment', button_color=('white', 'blue'), font=('Arial', buttFontSize), key='Environment')],
                 [sg.T()],
-                [sg.Button('Radio', button_color=('white', 'blue'), key='Radio')],
+                [sg.Button('Radio', button_color=('white', 'blue'), font=('Arial', buttFontSize), key='Radio')],
+                [sg.T()],
+                [sg.Button('Map', button_color=('white', 'blue'), font=('Arial', buttFontSize), key='Map')],
             ]
-for i in range(0, 3):
-    layout += [ [sg.T()], ]
-
 layout +=   [
-                [sg.T(' ' * 90), sg.Button('Exit', button_color=('white', 'firebrick4'), key='Exit')],
+                [sg.T()],
+                [sg.Button('Exit', button_color=('white', 'firebrick4'), font=('Arial', buttFontSize), key='Exit')],
             ]
 
 # ------ Window creation
-window = sg.Window('PipBoy500', layout, size=(480, 320), no_titlebar=True)
+window = sg.Window('PipBoy500', layout, size=screenSize, no_titlebar=True)
 
 # ------ Program Loop --------
 while True:
@@ -88,16 +94,23 @@ while True:
     event, values = window.Read(timeout=1000) # ------ Update window every second
     updateAll() # Update readings
     if event is None or event == 'Exit': # ------ Close out of the program
+        radio.playShutdown()
+        time.sleep(1)
         break
+
+    if event is not '__TIMEOUT__':
+        radio.playButtonNoise()
     
     # ------ Environment Window Stuff ------
     if event == 'Environment' and not envActive:
         envActive = True
         environmentLayout = getEnvironmentLayout()
-        environmentWindow = sg.Window('Environment', environmentLayout, size=(480, 320), no_titlebar=True)
+        environmentWindow = sg.Window('Environment', environmentLayout, size=screenSize, no_titlebar=True)
         while envActive:
             envEvent, envValues = environmentWindow.Read(timeout=1000)
-            environmentUpdate()
+            updateAll()
+            if envEvent is not '__TIMEOUT__':
+                radio.playButtonNoise()
             if envEvent is None or envEvent == 'Return':
                 environmentWindow.Hide()
                 envActive = False
@@ -107,20 +120,41 @@ while True:
     if event == 'Radio' and not radioActive:
         radioActive = True
         radioLayout = getRadioLayout()
-        radioWindow = sg.Window('Radio', radioLayout, size=(480, 320), no_titlebar=True)
+        radioWindow = sg.Window('Radio', radioLayout, size=screenSize, no_titlebar=True)
         while radioActive:
             radEvent, radValues = radioWindow.Read(timeout=1000)
-            radioUpdateAll()
-            if radEvent == 'Next':
-                radio.changeSong(1)
-                radEvent = 'Play'
-            if radEvent == 'Play':
-                radio.playSong()
-            if radEvent == 'Stop':
-                radio.stopSong()
-            if radEvent is None or radEvent == 'Return':
-                radioWindow.Hide()
-                radioActive = False
+            updateAll()
+            if radEvent is not '__TIMEOUT__':
+                if radEvent == 'Prev':
+                    radio.changeSong(-1)
+                    radio.playSong()
+                if radEvent == 'Play':
+                    radio.playSong()
+                if radEvent == 'Next':
+                    radio.changeSong(1)
+                    radio.playSong()
+                if radEvent == 'Stop':
+                    radio.stopSong()
+                if radEvent is None or radEvent == 'Return':
+                    radioWindow.Hide()
+                    radioActive = False
+                    break
+                radio.playClickNoise()
+                
+
+    # ------ Map Window Stuff ------
+    if event == 'Map' and not mapActive:
+        mapActive = True
+        mapLayout = getMapLayout()
+        mapWindow = sg.Window('Map', mapLayout, size=screenSize, no_titlebar=True)
+        while mapActive:
+            mapEvent, mapValues = mapWindow.Read(timeout=1000)
+            updateAll()
+            if mapEvent is not '__TIMEOUT__':
+                radio.playButtonNoise()
+            if mapEvent is None or mapEvent == 'Return':
+                mapWindow.Hide()
+                mapActive = False
                 break
 
 window.close()
--- Radio.py
+++ Radio.py
@@ -4,15 +4,18 @@ import glob
 # makes a list of music files
 musicList = glob.glob('./Music/*.mp3')
 
-class Radio:
+class RadioDevice:
 
     def __init__(self):
-        mixer.init()
+        mixer.init(22050, -16, 2, 4096)
         self.songIndex = 0
         self.numSongs = len(musicList)
         mixer.music.load(musicList[self.songIndex])
         self.playing = False
         self.currSongName = musicList[self.songIndex].replace('./Music/', '').replace('.mp3', '')
+        self.shutdownNoise = mixer.Sound('./SFX/shutdown.wav')
+        self.buttonNoise = mixer.Sound('./SFX/beep.wav')
+        self.clickNoise = mixer.Sound('./SFX/click.wav')
 
     def playSong(self):
         mixer.music.play()
@@ -20,14 +23,24 @@ class Radio:
         self.playing = True
 
     def changeSong(self, num):
+        self.songIndex += num
         # Reset to first song if end is reached
-        if self.songIndex == self.numSongs - 1:
+        if self.songIndex == self.numSongs:
             self.songIndex = 0
-        else:
-            self.songIndex += num
+        elif self.songIndex == -1:
+            self.songIndex = self.numSongs - 1
         mixer.music.load(musicList[self.songIndex])
 
     def stopSong(self):
         mixer.music.stop()
         self.playing = False
         self.currSongName = ''
+
+    def playShutdown(self):
+        self.shutdownNoise.play()
+
+    def playButtonNoise(self):
+        self.buttonNoise.play()
+    
+    def playClickNoise(self):
+        self.clickNoise.play()
